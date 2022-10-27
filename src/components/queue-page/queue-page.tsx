620e48912c4c130301857a9e301da4d881356011
Stas Basov <san4y@yandex.ru>
2022-10-28 00:03:51 +0300

    queue

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,10 +1,82 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import styles from "./queue-page.module.css";
+import { ElementStates } from "../../types/element-states";
+import { Queue } from "../../utils/utils";
+import { timeout } from "../../utils/utils";
+
+const queue = new Queue(7)
 
 export const QueuePage: React.FC = () => {
+  const [value, setValue] = React.useState("");
+  const inNewArr: string[] = Array.from({length: 7})
+  const [newArr, setNewArr] = React.useState(inNewArr)
+  const [isLoadind, setIsLoadind] = React.useState(false);
+
+  const onChange = (e: any) => {
+    setValue(e.target.value);
+  };
+
+  const enqueue = async () => {
+    queue.enqueue(value, setNewArr)
+    setValue("");
+    setIsLoadind(true);
+    await timeout(500);
+    setIsLoadind(false);
+  }
+
+  const dequeue = async () => {
+    setIsLoadind(true);
+    await timeout(500);
+    queue.dequeue(setNewArr);
+    setIsLoadind(false);
+  };
+
   return (
     <SolutionLayout title="Очередь">
-
+      <div className={styles.input}>
+        <Input
+          onChange={onChange}
+          isLimitText={true}
+          maxLength={4}
+          max={4}
+          type="text"
+        ></Input>
+        <Button
+        onClick={() => enqueue()}
+              extraClass="ml-12"
+              text="Добавить"
+            ></Button>
+        <Button
+        onClick={() => dequeue()}
+              extraClass="ml-12"
+              text="Удалить"
+            ></Button>
+            <Button
+              extraClass="ml-40"
+              text="Очистить"
+            ></Button>
+      </div>
+      <ul className="ul">
+        {newArr.map((obj, id) => {
+          return (
+            <div className={styles.circle} key={id}>
+              <Circle
+                key={id}
+                letter={obj}
+                state={
+                  isLoadind && id === newArr.length - 1
+                    ? ElementStates.Changing
+                    : ElementStates.Default
+                }
+              ></Circle>
+              <p>{id}</p>
+            </div>)
+        })}
+      </ul>
     </SolutionLayout>
   );
 };
--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,38 +1,41 @@
 import { ElementStates } from "../types/element-states";
 
 export const timeout = (milliseconds: number) => {
-    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
 };
 
 type TArr = {
-    obj: number;
-    color: ElementStates;
-  };
+  obj: number;
+  color: ElementStates;
+};
 
 export const swap = (
-    arr: TArr[],
-    firstIndex: number,
-    secondIndex: number
-  ): void => {
-    [arr[firstIndex], arr[secondIndex]] = [arr[secondIndex], arr[firstIndex]];
+  arr: TArr[],
+  firstIndex: number,
+  secondIndex: number
+): void => {
+  [arr[firstIndex], arr[secondIndex]] = [arr[secondIndex], arr[firstIndex]];
 };
 
 export class Stack<T> {
   private container: T[] = [];
 
-  push = (item: T, setNewArr: React.Dispatch<React.SetStateAction<T[]>>): void => {
+  push = (
+    item: T,
+    setNewArr: React.Dispatch<React.SetStateAction<T[]>>
+  ): void => {
     this.container.push(item);
-    setNewArr([...this.container])
+    setNewArr([...this.container]);
   };
 
   pop = (setNewArr: React.Dispatch<React.SetStateAction<T[]>>): void => {
     this.container.pop();
-    setNewArr([...this.container])
+    setNewArr([...this.container]);
   };
 
   clear = (setNewArr: React.Dispatch<React.SetStateAction<T[]>>): void => {
     this.container = [];
-    setNewArr([])
+    setNewArr([]);
   };
 
   peak = (): T | null => {
@@ -40,4 +43,53 @@ export class Stack<T> {
   };
 
   getSize = () => this.container.length;
+}
+
+export class Queue {
+  private container: string[] = [];
+  private head = 0;
+  private tail = 0;
+  private readonly size: number = 0;
+  private length: number = 0;
+
+  constructor(size: number) {
+    this.size = size;
+    this.container = Array(size);
+  }
+
+  enqueue = (item: string,
+    setNewArr: React.Dispatch<React.SetStateAction<string[]>>) => {
+    if (this.length >= this.size) {
+      throw new Error("Maximum length exceeded");
+    }
+    console.log(item)
+    this.container[this.tail] = item;
+    setNewArr([...this.container])
+    this.tail++;
+    this.length++;
+  };
+
+  dequeue = () => {
+    if (this.isEmpty()) {
+      throw new Error("No elements in the queue");
+    }
+
+    this.head++;
+    this.length--;
+  };
+  clear = () => {
+    this.head = 0;
+    this.tail = 0;
+    this.length = 0;
+  };
+
+  getHead = () => {
+    return this.head;
+  };
+
+  getTail = () => {
+    return this.tail;
+  };
+
+  isEmpty = () => this.length === 0;
 }
