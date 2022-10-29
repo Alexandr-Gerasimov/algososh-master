ae10bee23a6bbe93c9dd7f7b8107409ce3381d14
Stas Basov <san4y@yandex.ru>
2022-10-29 23:35:06 +0300

    list

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,10 +1,382 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import styles from "./list-page.module.css";
+import { ElementStates } from "../../types/element-states";
+import { ArrowIcon } from "../ui/icons/arrow-icon";
+import { LinkedList } from "../../utils/utils";
+import { timeout } from "../../utils/utils";
+
+type TArr = {
+  obj: string | null;
+  color: ElementStates;
+  head?: boolean;
+  tail?: boolean;
+  topCircle?: boolean;
+  bottomCircle?: boolean;
+  smallName?: string | null;
+};
+
+function getRandomFloat(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
 export const ListPage: React.FC = () => {
+  const [value, setValue] = React.useState("");
+  const [index, setIndex] = React.useState<number>(0);
+  const inNewArr: TArr[] = Array.from({ length: 4 }, () => ({
+    obj: String(getRandomFloat(0, 999)),
+    color: ElementStates.Default,
+    head: false,
+    tail: false,
+  }));
+  const linkedList = new LinkedList<string>();
+  const [newArr, setNewArr] = React.useState(inNewArr);
+  const [isLoadindAdd, setIsLoadindAdd] = React.useState(false);
+  const [isLoadind, setIsLoadind] = React.useState("");
+
+  const onValueChange = (e: any) => {
+    setValue(e.target.value);
+  };
+
+  const onIndexChange = (e: any) => {
+    setIndex(e.target.value);
+  };
+
+  const addHead = async () => {
+    setIsLoadind("addHead");
+    let listData = linkedList.listToData();
+    let newList = newArr;
+    await timeout(500);
+    newList[0] = {
+      ...newList[0],
+      topCircle: true,
+      smallName: value,
+    };
+    setNewArr([...newList]);
+    await timeout(500);
+    newList[0] = {
+      ...newList[0],
+      topCircle: false,
+    };
+    linkedList.addTail(value);
+    listData = linkedList.listToData();
+    for (let i = 0; i < listData.length; i++) {
+      newList.unshift({
+        obj: listData[i],
+        color: ElementStates.Modified,
+        head: true,
+        tail: false,
+      });
+      setNewArr([...newList]);
+      await timeout(500);
+      newList.shift();
+      newList.unshift({
+        obj: listData[i],
+        color: ElementStates.Default,
+        head: true,
+        tail: false,
+      });
+    }
+    setNewArr([...newList]);
+    setValue("");
+    setIsLoadind("");
+  };
+
+  const addTail = async () => {
+    setIsLoadind("addTail");
+    let listData = linkedList.listToData();
+    let newList = newArr;
+    await timeout(500);
+    newList[newList.length - 1] = {
+      ...newList[0],
+      bottomCircle: true,
+      smallName: value,
+    };
+    setNewArr([...newList]);
+    await timeout(500);
+    newList[newList.length - 1] = {
+      ...newList[0],
+      bottomCircle: false,
+    };
+    linkedList.addTail(value);
+    listData = linkedList.listToData();
+    for (let i = 0; i < listData.length; i++) {
+      newList.push({
+        obj: listData[i],
+        color: ElementStates.Modified,
+        head: false,
+        tail: true,
+      });
+      setNewArr([...newList]);
+      await timeout(500);
+      newList.pop();
+      newList.push({
+        obj: listData[i],
+        color: ElementStates.Default,
+        head: false,
+        tail: true,
+      });
+    }
+    await timeout(500);
+    setNewArr([...newList]);
+    setValue("");
+    setIsLoadind("");
+  };
+
+  const deleteHead = async () => {
+    setIsLoadind("deleteHead");
+    let listData = linkedList.listToData();
+    let newList = newArr;
+    await timeout(500);
+    newList[newList.length - 1] = {
+      ...newList[0],
+      topCircle: true,
+      obj: null,
+      smallName: newList[0].obj,
+    };
+    await timeout(500);
+    setNewArr([...newList]);
+    await timeout(500);
+    newList.shift();
+    setNewArr([...newList]);
+    linkedList.deleteHead();
+    listData = linkedList.listToData();
+    for (let i = 0; i < listData.length; i++) {
+      newList.push({
+        obj: listData[i],
+        color: ElementStates.Default,
+        head: true,
+        tail: false,
+      });
+    }
+    await timeout(500);
+    setNewArr([...newList]);
+    setIsLoadind("");
+  };
+
+  const deleteTail = async () => {
+    setIsLoadind("deleteTail");
+    let listData = linkedList.listToData();
+    let newList = newArr;
+    await timeout(500);
+    newList[newList.length - 1] = {
+      ...newList[0],
+      bottomCircle: true,
+      obj: null,
+      smallName: newList[newList.length - 1].obj,
+    };
+    await timeout(500);
+    setNewArr([...newList]);
+    await timeout(500);
+    newList.pop();
+    setNewArr([...newList]);
+    linkedList.deleteTail();
+    listData = linkedList.listToData();
+    for (let i = 0; i < listData.length; i++) {
+      newList.unshift({
+        obj: listData[i],
+        color: ElementStates.Default,
+        head: true,
+        tail: false,
+      });
+    }
+    await timeout(500);
+    setNewArr([...newList]);
+    setIsLoadind("");
+  };
+
+  const addIndex = async () => {
+    setIsLoadind("addIndex");
+    let listData = linkedList.listToData();
+    let newList = newArr;
+    await timeout(500);
+    for (let i = 0; i <= index; i++) {
+      newList[i] = {
+        ...newList[i],
+        topCircle: true,
+        smallName: value,
+      };
+      setNewArr([...newList]);
+      await timeout(500);
+      newList[i] = {
+        ...newList[i],
+        topCircle: false,
+      };
+      if(i<index) {
+      newList[i] = {
+        ...newList[i],
+        obj: newList[i].obj,
+        color: ElementStates.Changing,
+      };
+    }
+      setNewArr([...newList]);
+    }
+    await timeout(500);
+    newList.forEach((obj) => (obj.color = ElementStates.Default));
+    linkedList.addIndex(value, index);
+    listData = linkedList.listToData();
+
+    setNewArr([...newList]);
+    newArr.splice(index, 0, {
+      obj: value,
+      color: ElementStates.Modified,
+      head: true,
+      tail: true,
+    });
+    setNewArr([...newList]);
+    await timeout(500);
+    newList.forEach((obj) => (obj.color = ElementStates.Default));
+    setNewArr([...newList]);
+    setIsLoadind("");
+  };
+
+  const deleteIndex = async () => {
+    setIsLoadind("deleteIndex");
+    let listData = linkedList.listToData();
+    let newList = newArr;
+    setNewArr([...newList]);
+    for (let i = 0; i < index; i++) {
+      newList[i] = {
+        ...newList[i],
+        obj: newList[i].obj,
+        color: ElementStates.Changing,
+      };
+      await timeout(500);
+      setNewArr([...newList]);
+    }
+
+    newList[index] = {
+      ...newList[index],
+      bottomCircle: true,
+      obj: "",
+      smallName: newList[index].obj,
+    };
+    await timeout(500);
+    newList[index] = {
+      ...newList[index],
+      topCircle: false,
+    };
+    setNewArr([...newList]);
+
+    newList.forEach((obj) => (obj.color = ElementStates.Default));
+
+    linkedList.deleteIndex(index);
+    listData = linkedList.listToData();
+    newArr.splice(index, 1);
+    await timeout(500);
+    setNewArr([...newList]);
+    setIsLoadind("");
+  };
+
   return (
     <SolutionLayout title="Связный список">
-
+      <div className={styles.block}>
+        <Input
+          onChange={onValueChange}
+          maxLength={4}
+          max={4}
+          type="text"
+          extraClass={styles.input}
+        ></Input>
+        <Button
+          onClick={() => addHead()}
+          extraClass={styles.topButton}
+          text="Добавить в head"
+          disabled={value === "" || isLoadind !== ""}
+          isLoader={isLoadind === "addHead"}
+        ></Button>
+        <Button
+          onClick={() => addTail()}
+          extraClass={styles.topButton}
+          text="Добавить в tail"
+          disabled={value === "" || isLoadind !== ""}
+          isLoader={isLoadind === "addTail"}
+        ></Button>
+        <Button
+          onClick={() => deleteHead()}
+          extraClass={styles.topButton}
+          text="Удалить из head"
+          disabled={newArr.length === 0 || isLoadind !== ""}
+          isLoader={isLoadind === "deleteHead"}
+        ></Button>
+        <Button
+          onClick={() => deleteTail()}
+          extraClass={styles.topButton}
+          text="Удалить из tail"
+          disabled={newArr.length === 0 || isLoadind !== ""}
+          isLoader={isLoadind === "deleteTail"}
+        ></Button>
+      </div>
+      <div className={styles.block}>
+        <Input
+          onChange={onIndexChange}
+          maxLength={4}
+          max={4}
+          type="text"
+          extraClass={styles.input}
+        ></Input>
+        <Button
+          onClick={() => addIndex()}
+          extraClass={styles.button}
+          text="Добавить по индексу"
+          disabled={index === 0 || value === "" || isLoadind !== ""}
+          isLoader={isLoadind === "addIndex"}
+        ></Button>
+        <Button
+          onClick={() => deleteIndex()}
+          extraClass={styles.button}
+          text="Удалить по индексу"
+          disabled={index === 0 || value !== "" || isLoadind !== ""}
+          isLoader={isLoadind === "deleteIndex"}
+        ></Button>
+      </div>
+      <ul className={styles.ul}>
+        {newArr.map((obj, id) => {
+          return (
+            <div key={id} className={styles.list}>
+              <div className={styles.circle}>
+                {obj.topCircle && (
+                  <Circle
+                    isSmall={true}
+                    state={ElementStates.Changing}
+                    letter={obj.smallName}
+                    extraClass={styles.smallTop}
+                  />
+                )}
+                <Circle
+                  extraClass={styles.mainCircle}
+                  key={id}
+                  letter={obj.obj}
+                  state={obj.color}
+                  index={id}
+                  head={id === 0 && obj.topCircle !== true ? "Head" : ""}
+                  tail={
+                    id === newArr.length - 1 && obj.bottomCircle !== true
+                      ? "Tail"
+                      : ""
+                  }
+                ></Circle>
+                {obj.bottomCircle === true && (
+                  <Circle
+                    isSmall={true}
+                    state={ElementStates.Changing}
+                    letter={obj.smallName}
+                    extraClass={styles.smallBottom}
+                  />
+                )}
+              </div>
+              {id !== newArr.length - 1 && (
+                <div className={styles.arrow}>
+                  <ArrowIcon></ArrowIcon>
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </ul>
     </SolutionLayout>
   );
 };
--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -95,3 +95,140 @@ export class Queue {
 
   isEmpty = () => this.length === 0;
 }
+
+class NodeList<T> {
+  value: T;
+  next: NodeList<T> | null = null;
+
+  constructor(value: T, next: NodeList<T> | null) {
+  this.value = value;
+  this.next = (next === undefined ? null : next);
+  }
+}
+
+export class LinkedList<T> {
+  private head: NodeList<T> | null;
+  private tail: NodeList<T> | null;
+
+  constructor() {
+      this.head = null;
+      this.tail = null;
+  }
+
+  addHead(value: T) {
+      const newList = new NodeList(value, null);
+      if (!this.head) {
+          this.head = newList;
+      } else {
+        newList.next = this.head;
+        this.head = newList;
+      }
+  }
+
+  addTail(value: T) {
+      const newList = new NodeList(value, null);
+      if (!this.head || !this.tail) {
+          this.head = newList;
+          this.tail = newList;
+      } else {
+          this.tail.next = newList;
+          this.tail = newList;
+      }
+  }
+
+  deleteHead() {
+      if (this.head) {
+          if (this.head.next) {
+          this.head = this.head.next;
+          } else {
+          this.head = null;
+          }
+      }
+  }
+
+  deleteTail() {
+      if (this.head) {
+          if (this.head === this.tail) {
+          this.head = null;
+          this.tail = null;
+          } else {
+          let nextList = this.head.next;
+          let prev = this.head;
+          while (nextList !== this.tail && nextList) {
+              prev = nextList;
+              nextList = nextList.next;
+          }
+          prev.next = null;
+          this.tail = prev;
+          }
+      }
+  }
+
+  addIndex(value: T, index: number) {
+    if (this.head) {
+        const newList = new NodeList(value, null);
+        if (index === 0) {
+          newList.next = this.head;
+          this.head = newList;
+        } else {
+          let curr: NodeList<T> | null = this.head;
+          let currIndex = 0;
+          let prev = this.head;
+          while (currIndex < index) {
+            if(curr){
+              currIndex++
+              prev = curr;
+              curr = curr.next;
+            }
+          }
+          newList.next = curr;
+        if(prev){
+          prev.next = newList;}
+        }
+
+    }
+}
+
+  deleteIndex(index: number) {
+      if (this.head) {
+          if (index === 0) {
+          this.deleteHead();
+          } else {
+          let nextList = this.head.next;
+          let prev = this.head;
+          let i = index;
+          while ((nextList !== this.tail && nextList) && (i !== 1 && nextList)) {
+              i--;
+              prev = nextList;
+              nextList = nextList.next;
+          }
+          if (nextList) {
+              prev.next = nextList.next;
+          } else {
+              prev.next = null;
+          }
+          this.tail = prev;
+          }
+      }
+  }
+
+  listToData() {
+      const data:Array<T> = []
+      let node = this.head;
+      
+      while (node) {
+          data.push(node.value);
+          node = node.next;
+      }
+      return data;
+  }
+
+  getHead() {
+      return this.head;
+  }
+
+  getTail() {
+      return this.tail;
+  }
+
+} 
